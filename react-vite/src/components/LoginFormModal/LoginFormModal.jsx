52e25e3beae58bef43928320158d923c15a9d665
Alex Garbarino <alexiothebrave@gmail.com>
2025-04-02 21:32:53 -0500

    working css tweaks

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,60 +1,76 @@
-import { useState } from "react";
-import { thunkLogin } from "../../redux/session";
-import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import "./LoginForm.css";
+import { useState } from 'react';
+import { thunkLogin } from '../../redux/session';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import './LoginForm.css';
 
 function LoginFormModal() {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
+  const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
+  const minUserChars = 4;
+  const minPassChars = 6;
 
-  const handleSubmit = async (e) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    const serverResponse = await dispatch(
-      thunkLogin({
-        email,
-        password,
-      })
-    );
-
-    if (serverResponse) {
-      setErrors(serverResponse);
-    } else {
-      closeModal();
-    }
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors);
+        }
+      });
   };
 
+  const handleDemoLogin = () => {
+    return dispatch(sessionActions.login({ credential: 'demoUser', password: 'password' }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors);
+        }
+      });
+  };
+
+  const isButtonDisabled = credential.length < minUserChars || password.length < minPassChars;
+
   return (
-    <>
-      <h1>Log In</h1>
+    <div className="login-form-container">
+    <div className="login-form">
+      <h1 className="login-form__title">Log In</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Email
+        <div className="login-form__input-container">
           <input
             type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
             required
           />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
+          <label className={credential ? 'active' : ''}>Username or Email</label>
+        </div>
+        <div className="login-form__input-container">
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
+          <label className={password ? 'active' : ''}>Password</label>
+        </div>
+        {errors.credential && (
+          <p className="login-form__error">{errors.credential}</p>
+        )}
+        <button type="submit" disabled={isButtonDisabled}>Log In</button>
       </form>
-    </>
+      <button className="login-form__demo-user" onClick={handleDemoLogin}>Log In as Demo User</button>
+    </div>
+    </div>
   );
 }
 
