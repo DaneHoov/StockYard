6b2e1fe63a50855c4fa295485a37e64bfa83d6dd
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-03 20:34:04 -0400

    Added navigation styling

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,35 @@
-import { NavLink } from "react-router-dom";
-import ProfileButton from "./ProfileButton";
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import { GiHouse } from "react-icons/gi";
 import "./Navigation.css";
 
-function Navigation() {
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="nav-left">
+          <NavLink to="/" className="home-logo">
+            <GiHouse size={35} color="#000000" />
+          </NavLink>
+          <NavLink to="/" className="title">Stay Haven</NavLink>
+        </div>
+        <div className="nav-right">
+          {sessionUser && (
+            <div className="new-spot-link">
+              <NavLink to="/api/spots" className="create-link">
+                Create a New Spot
+              </NavLink>
+            </div>
+          )}
+          <div className="profile-btn-wrapper">
+            {isLoaded && <ProfileButton user={sessionUser} />}
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
