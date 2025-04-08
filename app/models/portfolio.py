d80704193c78655a0b60b9129c6400613e545966
anthonys0102 <anthonysantos444@gmail.com>
2025-04-08 14:23:05 -0500

    added to models

---
+++ app/models/portfolio.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Portfolio(db.Model):
+    __tablename__ = 'portfolios'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False, unique=True)
+    cash_balance = db.Column(db.Float, default=0.0)
+
+    user = db.relationship("User", back_populates="portfolio")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'cash_balance': self.cash_balance
+        }
