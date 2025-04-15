73157a98b489a3184323d7ebf0c7d213aa2cf160
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-14 20:11:11 -0400

    Modified login styling to match signup

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+=======
+from .db import db
+>>>>>>> ef081c6 (Modified login styling to match signup)
 
 class Portfolio(db.Model):
     __tablename__ = 'portfolios'
 
+<<<<<<< HEAD
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
@@ -21,4 +26,17 @@ class Portfolio(db.Model):
             "id": self.id,
             "user_id": self.user_id,
             "balance": self.balance
+=======
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    balance = db.Column(db.Float, nullable=False, default=0.0)
+
+    user = db.relationship('User', back_populates='portfolios')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'balance': self.balance,
+>>>>>>> ef081c6 (Modified login styling to match signup)
         }
