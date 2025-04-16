2268bc73f476341e8f1953a21ee9d02d3c380459
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-15 20:29:27 -0400

    Rebased

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -1,17 +1,20 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Portfolio(db.Model):
     __tablename__ = 'portfolios'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    balance = db.Column(db.Float, nullable=False, default=0.0)
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
-    user = db.relationship('User', back_populates='portfolios')
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False, unique=True)
+    cash_balance = db.Column(db.Float, default=0.0)
+
+    user = db.relationship("User", back_populates="portfolio")
 
     def to_dict(self):
         return {
             'id': self.id,
             'user_id': self.user_id,
-            'balance': self.balance,
+            'cash_balance': self.cash_balance
         }
