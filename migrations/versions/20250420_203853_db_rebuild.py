6d6a9875b838a80aee3fb5c50509f9daf2817972
Alex Garbarino <alexiothebrave@gmail.com>
2025-04-20 21:09:25 -0500

    rebuilt db

--- migrations/versions/20250420_203853_db_rebuild.py
+++ migrations/versions/20250420_203853_db_rebuild.py
@@ -1,8 +1,8 @@
-"""empty message
+"""db rebuild
 
-Revision ID: a115b71f2552
+Revision ID: 2f6d9bd21137
 Revises: 
-Create Date: 2025-04-20 21:10:14.104748
+Create Date: 2025-04-20 20:38:53.255762
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a115b71f2552'
+revision = '2f6d9bd21137'
 down_revision = None
 branch_labels = None
 depends_on = None
