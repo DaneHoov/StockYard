4a994a5a1363129296958981f542444fc200fde8
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-21 18:13:03 -0400

    Rebuilt db

--- migrations/versions/20250421_181026_rebuild_db.py
+++ migrations/versions/20250421_181026_rebuild_db.py
@@ -1,8 +1,8 @@
-"""empty message
+"""rebuild db
 
-Revision ID: 01da6f0d84eb
+Revision ID: 0075cbdfae6e
 Revises: 
-Create Date: 2025-04-21 13:42:51.010560
+Create Date: 2025-04-21 18:10:26.948902
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '01da6f0d84eb'
+revision = '0075cbdfae6e'
 down_revision = None
 branch_labels = None
 depends_on = None
