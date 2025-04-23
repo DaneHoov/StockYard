b688bf15924395b9ebe49bfdfdd1c7122b071f71
anthonys0102 <anthonysantos444@gmail.com>
2025-04-23 12:41:39 -0500

    instance

--- migrations/versions/20250422_192005_.py
+++ migrations/versions/20250422_192005_.py
@@ -1,8 +1,8 @@
-"""rebuild db
+"""empty message
 
-Revision ID: e9a1f941172c
+Revision ID: b0c8f5a8eab1
 Revises: 
-Create Date: 2025-04-22 18:23:09.096779
+Create Date: 2025-04-22 19:20:05.610359
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e9a1f941172c'
+revision = 'b0c8f5a8eab1'
 down_revision = None
 branch_labels = None
 depends_on = None
