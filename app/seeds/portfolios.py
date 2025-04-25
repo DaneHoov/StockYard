59a15ce9f40a1c957cbbfd046fcdaa2b530ae009
danehoov <DaneHoov22@gmail.com>
2025-04-25 15:48:43 -0600

    more small changes

--- app/seeds/portfolios.py
+++ app/seeds/portfolios.py
@@ -2,8 +2,8 @@ from app.models import db, Portfolio, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_portfolios():
-    portfolio1 = Portfolio(user_id=demo_user.id, balance=10000.0)
-    portfolio2 = Portfolio(user_id=marnie_user.id, balance=5000.0)
+    portfolio1 = Portfolio(user_id=1, balance=10000.0)
+    portfolio2 = Portfolio(user_id=2, balance=5000.0)
     db.session.add_all([portfolio1, portfolio2])
     db.session.commit()
 
