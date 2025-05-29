dea1678b34879232c1d4fa6acaf2f94c9c783f77
danehoov <DaneHoov22@gmail.com>
2025-05-28 19:43:39 -0500

    restore environment and schema for imports

--- app/models/db.py
+++ app/models/db.py
@@ -3,15 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# environment = os.getenv("FLASK_ENV")
-# SCHEMA = os.environ.get("SCHEMA")
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 
 # helper function for adding prefix to foreign key column references in production
 def add_prefix_for_prod(attr):
-    environment = os.getenv("FLASK_ENV")
-    schema = os.getenv("SCHEMA")
-    if environment == "production" and schema:
-        return f"{schema}.{attr}"
+    if environment == "production" and SCHEMA:
+        return f"{SCHEMA}.{attr}"
     return attr
