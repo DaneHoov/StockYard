83a9654e8052c02aab31a96cbb90e09d1cd1a2d3
danehoov <DaneHoov22@gmail.com>
2025-05-28 19:37:18 -0500

    dynamically load SCHEMA and env in add prefix for prod

--- app/models/db.py
+++ app/models/db.py
@@ -1,15 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
-
 import os
-environment = os.getenv("FLASK_ENV")
-SCHEMA = os.environ.get("SCHEMA")
-
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# environment = os.getenv("FLASK_ENV")
+# SCHEMA = os.environ.get("SCHEMA")
+
+
+
 # helper function for adding prefix to foreign key column references in production
 def add_prefix_for_prod(attr):
-    if environment == "production":
-        return f"{SCHEMA}.{attr}"
-    else:
-        return attr
+    environment = os.getenv("FLASK_ENV")
+    schema = os.getenv("SCHEMA")
+    if environment == "production" and schema:
+        return f"{schema}.{attr}"
+    return attr
